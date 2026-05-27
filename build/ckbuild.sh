e31c7118c2dab749b0cfc245416da324be46cc0c
elitetest1 <tizzyprove@gmail.com>
2026-05-27 00:11:12 -0300

    build: add tag "for EliteUI" and also add a new version

--- build/ckbuild.sh
+++ build/ckbuild.sh
@@ -60,7 +60,7 @@ OUT_DTBIMAGE="$TMPDIR/dtb.img"
 MKBOOTIMG="$(pwd)/build/mkbootimg/mkbootimg.py"
 MKDTBOIMG="$(pwd)/build/dtb/mkdtboimg.py"
 
-FK_VER="v1.1.1"
+FK_VER="For-EliteUI-v1.1.2"
 USE_CCACHE="${USE_CCACHE:-1}"
 DO_TAR="${DO_TAR:-1}"
 DO_ZIP="${DO_ZIP:-1}"
